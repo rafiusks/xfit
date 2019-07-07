14c3d5ee1433908792302cdca64b1001d7dc46ac
Rafael Vidal <vidal@live.com>
2019-07-07 16:40:05 +0800

    Second li added

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="#" target="_blank" rel="noopener noreferrer">Home</a></li>
+    </ul>
+    <ul>
+        <li><a href="#" target="_blank" rel="noopener noreferrer">Home</a></li>
+    </ul>
+</nav>
