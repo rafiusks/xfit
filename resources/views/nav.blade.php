2f169ff14d92407da6f0f476d332236003500401
Rafael Vidal <rafael@Rafaels-MacBook-Pro.local>
2019-07-07 17:05:06 +0800

    third li added

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -5,4 +5,7 @@
     <ul>
         <li><a href="#" target="_blank" rel="noopener noreferrer">Home</a></li>
     </ul>
+    <ul>
+        <li><a href="#" target="_blank" rel="noopener noreferrer">Home</a></li>
+    </ul>
 </nav>
